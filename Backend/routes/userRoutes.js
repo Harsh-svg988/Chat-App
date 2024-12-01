ec256af17fc35fc36ed02cd6a4f55128b79144ba
Harsh-svg988 <harshprabhakar305@gmail.com>
2024-12-01 12:44:38 +0530

    Done with backend

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express'
-import protectRoute from '../middlewares/protectRoute';
-const router = express.Router
+import protectRoute from '../middlewares/protectRoute.js';
+import { getUsersForSidebar } from '../controllers/userController.js';
+const router = express.Router()
 
 router.get("/",protectRoute,getUsersForSidebar)
 export default router;
