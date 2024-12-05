30a5a2467b0d27d37609aba592036e4b86260c4f
Harsh-svg988 <harshprabhakar305@gmail.com>
2024-12-05 18:27:21 +0530

    Connected Backend with Fronted & Done with sigup functionality

---
+++ Frontend/src/hooks/useSignup.js
@@ -0,0 +1,50 @@
+import toast from "react-hot-toast";
+import { useState } from "react";
+
+const useSignUp = () =>{
+    const [loading,setLoading] = useState(false);
+    const signUp = async({fullname,username,password,confirmPassword,gender})=>{
+        const success = handleInputError({fullname,username,password,confirmPassword,gender})
+        console.log({fullname, username, password, confirmPassword, gender});
+        if(!success)return;
+        setLoading(true)
+        try {
+            const res = await fetch("http://localhost:8081/api/auth/signup",{
+                method:"POST",
+                headers:{"Content-Type":"application/json"},
+                body:JSON.stringify({fullname,username,password,confirmPassword,gender})
+            })
+            const data = await res.json();
+            console.log(data)
+            
+        } catch (error) {
+            toast.error(error.message)
+        } finally{
+            setLoading(false)
+        }
+
+    }
+    return {signUp,loading}
+}
+
+export default useSignUp;
+
+
+function handleInputError({fullname,username,password,confirmPassword,gender}){
+    if(!fullname || !username || !password || !confirmPassword || !gender){
+        toast.error("Please fill all the fields")
+        return false
+    }
+    if (password !== confirmPassword) {
+		toast.error("Passwords do not match");
+		return false;
+	}
+
+	if (password.length < 6) {
+		toast.error("Password must be at least 6 characters");
+		return false;
+	}
+
+	return true;
+
+}
