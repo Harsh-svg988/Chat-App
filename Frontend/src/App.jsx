cc4d0ad18a38a4d45f186a6f138908ffb6acb784
Harsh-svg988 <harshprabhakar305@gmail.com>
2024-12-06 00:40:51 +0530

    Fixed minor bug for Navigate in App.js

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,12 +5,12 @@ import './App.css'
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/SignUp'
 import Home from './pages/home/Home'
-import {Routes, Route } from 'react-router-dom'
+import {Routes, Route,Navigate } from 'react-router-dom'
 import {Toaster} from 'react-hot-toast'
 import { useAuthContext } from './Context/AuthContext'
 
 function App() {
-  const{authuser} = useAuthContext()
+  const{authUser} = useAuthContext()
   
 
   return (
@@ -18,7 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={authuser ? <Navigate to = "/"/> :<SignUp/>}/>
+        <Route path="/signup" element={authUser ? <Navigate to = "/"/> :<SignUp/>}/>
       </Routes>
       <Toaster/>
     </div>
