755ec5739f8f77b41b7fdec88ec07ed90b782deb
Harsh-svg988 <harshprabhakar305@gmail.com>
2024-12-05 18:26:17 +0530

    Used Cors

--- Backend/server.js
+++ Backend/server.js
@@ -5,14 +5,14 @@ import cookieParser from 'cookie-parser';
 import connectToMongoDB from './db/connectToMongoDb.js';
 import messageRoutes from "./routes/messageRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
-
+import cors from 'cors';
 
 dotenv.config();
 const app = express()
 const PORT = process.env.PORT || 5000
 console.log(PORT);
 
-
+app.use(cors());
 app.use(cookieParser())
 app.use(express.json())   // To parse the incoming requests with JSON payloads (from req.body)
 
