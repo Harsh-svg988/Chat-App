d2435f2a5fcb118c9d701ebe0194f027e0923e3c
Harsh-svg988 <harshprabhakar305@gmail.com>
2024-12-18 01:30:09 +0530

    Deployment bugs

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
-import { AuthContextProvider } from './Context/AuthContext.jsx'
-import { SocketContextProvider } from './Context/SocketContext.jsx'
+import { AuthContextProvider } from './context/AuthContext.jsx'
+import { SocketContextProvider } from './context/SocketContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
