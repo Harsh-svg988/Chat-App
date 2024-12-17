aacf9436e1bdc856fc09321347b3d1dea1afd8f3
Harsh-svg988 <harshprabhakar305@gmail.com>
2024-12-17 11:31:24 +0530

    Deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import authRoutes from "./routes/authRoutes.js"
 import cookieParser from 'cookie-parser';
 import connectToMongoDB from './db/connectToMongoDb.js';
@@ -15,17 +16,22 @@ dotenv.config();
 const PORT = process.env.PORT || 5000
 console.log(PORT);
 
+const __dirname = path.resolve();
+
 // app.use(cors());
 app.use(cookieParser())
 app.use(express.json())   // To parse the incoming requests with JSON payloads (from req.body)
 
-app.get("/",(req,res)=>{
-    res.send("Hello world Good Morning")
-})
 
 app.use("/api/messages",messageRoutes)
 app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
+
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
+});
 
 server.listen(8081,()=>{
     connectToMongoDB()
