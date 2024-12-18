ea4555198b79277acd643abe4713f8432c2bcb76
Harsh-svg988 <harshprabhakar305@gmail.com>
2024-12-18 17:33:13 +0530

    Socket url updated

--- Frontend/src/contexts/SocketContext.jsx
+++ Frontend/src/contexts/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:8081",{
+            const socket = io("https://chat-app-eem1.onrender.com/",{
                 query:{
                     userId:authUser._id
                 }
